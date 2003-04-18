e29eed6922d67fe9ce9976e8668c6adef6e611b0
tcsmith <>
2003-04-18 22:25:30 +0000

    Initial version.

---
+++ src/rootlogger.cxx
@@ -0,0 +1,56 @@
+// Module:  Log4CPLUS
+// File:    rootlogger.cxx
+// Created: 4/2003
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+// $Log: not supported by cvs2svn $
+
+#include <log4cplus/spi/rootlogger.h>
+
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+using namespace log4cplus::spi;
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// RootLogger Constructor
+//////////////////////////////////////////////////////////////////////////////
+
+RootLogger::RootLogger(Hierarchy& h, LogLevel ll)
+: LoggerImpl(LOG4CPLUS_TEXT("root"), h)
+{
+    setLogLevel(ll);
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Logger Methods
+//////////////////////////////////////////////////////////////////////////////
+
+LogLevel 
+RootLogger::getChainedLogLevel()
+{
+    return ll;
+}
+
+
+void 
+RootLogger::setLogLevel(LogLevel ll)
+{
+    if(ll == NOT_SET_LOG_LEVEL) {
+        getLogLog().error("You have tried to set NOT_SET_LOG_LEVEL to root.");
+    }
+    else {
+        LoggerImpl::setLogLevel(ll);
+    }
+}
+
