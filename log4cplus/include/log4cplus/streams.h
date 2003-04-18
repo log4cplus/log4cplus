d633da015b442e3cbc0250a413a79c5373945032
tcsmith <>
2003-04-18 20:54:16 +0000

    Initial version.

---
+++ log4cplus/include/log4cplus/streams.h
@@ -0,0 +1,41 @@
+// Module:  Log4CPLUS
+// File:    ostream.h
+// Created: 4/2003
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+
+/** @file */
+
+#ifndef LOG4CPLUS_IOSTREAM_HEADER_
+#define LOG4CPLUS_IOSTREAM_HEADER_
+
+#include <log4cplus/config.h>
+
+#include <iostream>
+#include <sstream>
+
+#ifdef UNICODE
+    namespace log4cplus {
+        typedef std::wostream tostream;
+        typedef std::wostringstream tostringstream;
+        static tostream &tcout = std::wcout;
+        static tostream &tcerr = std::wcerr;
+    }
+#else
+    namespace log4cplus {
+        typedef std::ostream tostream;
+        typedef std::ostringstream tostringstream;
+        static tostream &tcout = std::cout;
+        static tostream &tcerr = std::cerr;
+    }
+#endif // UNICODE
+
+#endif // LOG4CPLUS_IOSTREAM_HEADER_
+
---
+++ log4cplus/include/log4cplus/tstring.h
@@ -0,0 +1,58 @@
+// Module:  Log4CPLUS
+// File:    tstring.h
+// Created: 4/2003
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+
+/** @file */
+
+#ifndef LOG4CPLUS_TSTRING_HEADER_
+#define LOG4CPLUS_TSTRING_HEADER_
+
+#include <log4cplus/config.h>
+#include <string>
+
+#ifdef UNICODE
+#  define LOG4CPLUS_TEXT(STRING) L##STRING
+#else
+#  define LOG4CPLUS_TEXT(STRING) STRING
+#endif // UNICODE
+
+
+#ifdef UNICODE
+namespace log4cplus {
+    typedef wchar_t tchar;
+    typedef std::wstring tstring;
+
+    namespace helpers {
+        std::string tostring(const std::wstring&);
+        std::wstring towstring(const std::string&);
+    }
+
+}
+
+#define LOG4CPLUS_C_STR_TO_TSTRING(STRING) log4cplus::helpers::towstring(STRING)
+#define LOG4CPLUS_STRING_TO_TSTRING(STRING) log4cplus::helpers::towstring(STRING)
+#define LOG4CPLUS_TSTRING_TO_STRING(STRING) log4cplus::helpers::tostring(STRING)
+
+#else
+namespace log4cplus {
+    typedef char tchar;
+    typedef std::string tstring;
+}
+
+#define LOG4CPLUS_C_STR_TO_TSTRING(STRING) std::string(STRING)
+#define LOG4CPLUS_STRING_TO_TSTRING(STRING) STRING
+#define LOG4CPLUS_TSTRING_TO_STRING(STRING) STRING
+
+#endif // UNICODE
+
+#endif // LOG4CPLUS_TSTRING_HEADER_
+
