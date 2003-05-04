431bf9768742b13f31b056ef9d38c22cb17e4c11
tcsmith <>
2003-05-04 07:25:16 +0000

    Initial version.

---
+++ src/loggingserver.cxx
@@ -0,0 +1,102 @@
+// Module:  LOG4CPLUS
+// File:    loggingserver.cxx
+// Created: 5/2003
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+// $Log: not supported by cvs2svn $
+
+#include <log4cplus/config.h>
+#include <log4cplus/consoleappender.h>
+#include <log4cplus/socketappender.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/helpers/socket.h>
+#include <log4cplus/helpers/threads.h>
+#include <log4cplus/spi/loggingevent.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+using namespace log4cplus::thread;
+
+
+namespace loggingserver {
+    class ClientThread : public AbstractThread {
+    public:
+        ClientThread(SharedAppenderPtr appender, Socket clientsock)
+        : appender(appender), clientsock(clientsock) 
+        {
+            cout << "Received a client connection!!!!" << endl;
+        }
+
+        ~ClientThread()
+        {
+            cout << "Client connection closed." << endl;
+        }
+
+        virtual void run();
+    private:
+        SharedAppenderPtr appender;
+        Socket clientsock;
+    };
+
+}
+
+
+
+
+int
+main()
+{
+    SharedAppenderPtr append_1(new ConsoleAppender());
+    append_1->setName("First");
+    append_1->setLayout( std::auto_ptr<Layout>(new log4cplus::TTCCLayout()) );
+
+    ServerSocket serverSocket(9998);
+    while(1) {
+        loggingserver::ClientThread *thr = 
+            new loggingserver::ClientThread(append_1, serverSocket.accept());
+        thr->start();
+    }
+
+    return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// loggingserver::ClientThread implementation
+////////////////////////////////////////////////////////////////////////////////
+
+
+void
+loggingserver::ClientThread::run()
+{
+    while(1) {
+        if(!clientsock.isOpen()) {
+            return;
+        }
+        SocketBuffer msgSizeBuffer(sizeof(unsigned int));
+        if(!clientsock.read(msgSizeBuffer)) {
+            return;
+        }
+
+        unsigned int msgSize = msgSizeBuffer.readInt();
+	cout << "ClientThread::run()- Message size: " << msgSize << endl;
+
+        SocketBuffer buffer(msgSize);
+        if(!clientsock.read(buffer)) {
+            return;
+        }
+        appender->doAppend( readFromBuffer(buffer) );   
+    }
+}
+
+
---
+++ src/socket.cxx
@@ -0,0 +1,208 @@
+// Module:  Log4CPLUS
+// File:    socket-win32.cxx
+// Created: 4/2003
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+// $Log: not supported by cvs2svn $
+
+#include <log4cplus/helpers/socket.h>
+#include <log4cplus/helpers/loglog.h>
+
+
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+
+#ifndef WIN32
+#  include <errno.h>
+#  include <unistd.h>
+#  define GET_LAST_ERROR errno
+#else
+#  define GET_LAST_ERROR WSAGetLastError()
+#endif
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// AbstractSocket ctors and dtor
+//////////////////////////////////////////////////////////////////////////////
+
+log4cplus::helpers::AbstractSocket::AbstractSocket()
+: sock(INVALID_SOCKET),
+  state(not_opened),
+  err(0)
+{
+}
+
+
+
+log4cplus::helpers::AbstractSocket::AbstractSocket(SOCKET_TYPE sock, SocketState state, int err)
+: sock(sock),
+  state(state),
+  err(err)
+{
+}
+
+
+
+log4cplus::helpers::AbstractSocket::AbstractSocket(const log4cplus::helpers::AbstractSocket& rhs)
+{
+    copy(rhs);
+}
+
+
+log4cplus::helpers::AbstractSocket::~AbstractSocket()
+{
+    close();
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// AbstractSocket methods
+//////////////////////////////////////////////////////////////////////////////
+
+void
+log4cplus::helpers::AbstractSocket::close()
+{
+    if(sock != INVALID_SOCKET) {
+        closeSocket(sock);
+        sock = INVALID_SOCKET;
+    }
+}
+
+
+
+bool
+log4cplus::helpers::AbstractSocket::isOpen() const
+{
+    return sock != INVALID_SOCKET;
+}
+
+
+
+
+log4cplus::helpers::AbstractSocket&
+log4cplus::helpers::AbstractSocket::operator=(const log4cplus::helpers::AbstractSocket& rhs)
+{
+    if(&rhs != this) {
+        close();
+        copy(rhs);
+    }
+
+    return *this;
+}
+
+
+
+void
+log4cplus::helpers::AbstractSocket::copy(const log4cplus::helpers::AbstractSocket& r)
+{
+    AbstractSocket& rhs = const_cast<AbstractSocket&>(r);
+    sock = rhs.sock;
+    state = rhs.state;
+    err = rhs.err;
+    rhs.sock = INVALID_SOCKET;
+    rhs.state = not_opened;
+    err = 0;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Socket ctors and dtor
+//////////////////////////////////////////////////////////////////////////////
+
+log4cplus::helpers::Socket::Socket()
+: AbstractSocket()
+{
+}
+
+
+
+log4cplus::helpers::Socket::Socket(const tstring& address, int port)
+: AbstractSocket()
+{
+    sock = connectSocket(address, port, state);
+    if(sock == INVALID_SOCKET) {
+        err = errno;
+    }
+}
+
+
+log4cplus::helpers::Socket::Socket(SOCKET_TYPE sock, SocketState state, int err)
+: AbstractSocket(sock, state, err)
+{
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Socket methods
+//////////////////////////////////////////////////////////////////////////////
+
+bool
+log4cplus::helpers::Socket::read(SocketBuffer& buffer)
+{
+    int retval = log4cplus::helpers::read(sock, buffer);
+    if(retval <= 0) {
+        close();
+    }
+    else {
+        buffer.setSize(retval);
+    }
+
+    return (retval > 0);
+}
+
+
+
+bool
+log4cplus::helpers::Socket::write(const SocketBuffer& buffer)
+{
+    int retval = log4cplus::helpers::write(sock, buffer);
+    if(retval <= 0) {
+        close();
+    }
+
+    return (retval > 0);
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// ServerSocket ctor and dtor
+//////////////////////////////////////////////////////////////////////////////
+
+log4cplus::helpers::ServerSocket::ServerSocket(int port)
+{
+    sock = openSocket(port, state);
+    if(sock == INVALID_SOCKET) {
+        err = errno;
+    }
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// ServerSocket methods
+//////////////////////////////////////////////////////////////////////////////
+
+log4cplus::helpers::Socket
+log4cplus::helpers::ServerSocket::accept()
+{
+    SocketState state;
+    SOCKET_TYPE clientSock = acceptSocket(sock, state);
+    return Socket(clientSock, state, 0);
+}
+
+
