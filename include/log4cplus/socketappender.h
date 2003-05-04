847781d24ceb9f66efe6329da50440f9f98b4fda
tcsmith <>
2003-05-04 01:06:22 +0000

    Initial version.

---
+++ include/log4cplus/socketappender.h
@@ -0,0 +1,97 @@
+// Module:  LOG4CPLUS
+// File:    socketappender.h
+// Created: 5/2003
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+
+/** @file */
+
+#ifndef _LOG4CPLUS_SOCKET_APPENDER_HEADER_
+#define _LOG4CPLUS_SOCKET_APPENDER_HEADER_
+
+#include <log4cplus/config.h>
+#include <log4cplus/appender.h>
+#include <log4cplus/helpers/socket.h>
+
+#define LOG4CPLUS_MAX_MESSAGE_SIZE (8*1024)
+
+
+namespace log4cplus {
+
+    /**
+     * Sends {@link LoggingEvent} objects to a remote a log server.
+     *
+     * <p>The SocketAppender has the following properties:
+     *
+     * <ul>
+     *   <p><li>Remote logging is non-intrusive as far as the log event is
+     *   concerned. In other words, the event will be logged with the same
+     *   time stamp, {@link org.apache.log4j.NDC}, location info as if it
+     *   were logged locally by the client.
+     *
+     *   <p><li>SocketAppenders do not use a layout.
+     *
+     *   <p><li>Remote logging uses the TCP protocol. Consequently, if
+     *   the server is reachable, then log events will eventually arrive
+     *   at the server.
+     *
+     *   <p><li>If the remote server is down, the logging requests are
+     *   simply dropped. However, if and when the server comes back up,
+     *   then event transmission is resumed transparently. This
+     *   transparent reconneciton is performed by a <em>connector</em>
+     *   thread which periodically attempts to connect to the server.
+     *
+     *   <p><li>Logging events are automatically <em>buffered</em> by the
+     *   native TCP implementation. This means that if the link to server
+     *   is slow but still faster than the rate of (log) event production
+     *   by the client, the client will not be affected by the slow
+     *   network connection. However, if the network connection is slower
+     *   then the rate of event production, then the client can only
+     *   progress at the network rate. In particular, if the network link
+     *   to the the server is down, the client will be blocked.
+     *
+     *   <p>On the other hand, if the network link is up, but the server
+     *   is down, the client will not be blocked when making log requests
+     *   but the log events will be lost due to server unavailability.
+     */
+    class LOG4CPLUS_EXPORT SocketAppender : public Appender {
+    public:
+      // Ctors
+        SocketAppender(const log4cplus::tstring& host, int port, 
+                       const log4cplus::tstring& serverName = "");
+        SocketAppender(log4cplus::helpers::Properties properties);
+
+      // Dtor
+        ~SocketAppender();
+
+      // Methods
+        virtual void close();
+
+    protected:
+        void openSocket();
+        virtual void append(const spi::InternalLoggingEvent& event);
+
+      // Data
+        log4cplus::helpers::Socket socket;
+        log4cplus::tstring host;
+        int port;
+        log4cplus::tstring serverName;
+    };
+
+    namespace helpers {
+        SocketBuffer convertToBuffer(const log4cplus::spi::InternalLoggingEvent& event,
+                                     const log4cplus::tstring& serverName);
+        log4cplus::spi::InternalLoggingEvent readFromBuffer(SocketBuffer& buffer);
+    } // end namespace helpers
+
+} // end namespace log4cplus
+
+#endif // _LOG4CPLUS_SOCKET_APPENDER_HEADER_
+
