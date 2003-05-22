7e8dcd5ee707ce3783b01bb3dd9aafae4e1f9ed4
tcsmith <>
2003-05-22 21:17:32 +0000

    Moved the sleep() method into sleep.cxx

--- log4cplus/src/threads.cxx
+++ log4cplus/src/threads.cxx
@@ -11,6 +11,10 @@
 // distribution in the LICENSE.APL file.
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.8  2003/04/29 17:38:45  tcsmith
+// Added "return NULL" to the threadStartFunc() method to make the Sun Forte
+// compiler happy.
+//
 // Revision 1.7  2003/04/18 21:58:47  tcsmith
 // Converted from std::string to log4cplus::tstring.
 //
@@ -91,44 +95,6 @@ log4cplus::thread::yield()
 #endif
 }
 
-
-#define MILLIS_TO_NANOS 1000000
-#define SEC_TO_MILLIS 1000
-#define MAX_SLEEP_SECONDS (DWORD)4294966        // (2**32-2)/1000
-
-void
-log4cplus::thread::sleep(unsigned long secs, unsigned long nanosecs)
-{
-#ifdef LOG4CPLUS_USE_PTHREADS
-    timespec sleep_time = { secs, nanosecs };
-    timespec remain;
-    while (nanosleep(&sleep_time, &remain)) {
-        if (errno == EINTR) {
-            sleep_time.tv_sec  = remain.tv_sec;
-            sleep_time.tv_nsec = remain.tv_nsec;               
-            continue;
-        }
-        else {
-            return;
-        }
-    }
-#endif
-#ifdef LOG4CPLUS_USE_WIN32_THREADS
-    DWORD nano_millis = nanosecs / static_cast<unsigned long>(MILLIS_TO_NANOS);
-    if (secs <= MAX_SLEEP_SECONDS) {
-        Sleep((secs * SEC_TO_MILLIS) + nano_millis);
-        return;
-    }
-        
-    DWORD no_of_max_sleeps = secs / MAX_SLEEP_SECONDS;
-            
-    for(DWORD i = 0; i < no_of_max_sleeps; i++) {
-        Sleep(MAX_SLEEP_SECONDS * 1000);
-    }
-               
-    Sleep((secs % MAX_SLEEP_SECONDS) * SEC_TO_MILLIS + nano_millis);
-#endif
-}
 
 
 #ifdef LOG4CPLUS_USE_PTHREADS
