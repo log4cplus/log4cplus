4582fb18e4c6541740a29624b78584782681a2d2
tcsmith <>
2003-05-22 21:11:27 +0000

    Initial version.

---
+++ log4cplus/include/log4cplus/helpers/sleep.h
@@ -0,0 +1,30 @@
+// Module:  Log4CPLUS
+// File:    threads.h
+// Created: 6/2001
+// Author:  Tad E. Smith
+//
+//
+// Copyright (C) Tad E. Smith  All rights reserved.
+//
+// This software is published under the terms of the Apache Software
+// License version 1.1, a copy of which has been included with this
+// distribution in the LICENSE.APL file.
+//
+
+/** @file */
+
+#ifndef _LOG4CPLUS_HELPERS_SLEEP_HEADER_
+#define _LOG4CPLUS_HELPERS_SLEEP_HEADER_
+
+#include <log4cplus/config.h>
+
+
+namespace log4cplus {
+    namespace helpers {
+        LOG4CPLUS_EXPORT void sleep(unsigned long secs, 
+                                    unsigned long nanosecs = 0);
+    } // end namespace helpers
+} // end namespace log4cplus
+
+#endif // _LOG4CPLUS_HELPERS_SLEEP_HEADER_
+
