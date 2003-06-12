aed852ef6c4968698e712e87f802f557584c40d1
tcsmith <>
2003-06-12 23:55:49 +0000

    Initial implementation of the DailyRollingFileAppender class.

--- log4cplus/src/fileappender.cxx
+++ log4cplus/src/fileappender.cxx
@@ -11,6 +11,9 @@
 // distribution in the LICENSE.APL file.
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.10  2003/06/06 17:04:31  tcsmith
+// Changed the ctor to take a 'const' Properties object.
+//
 // Revision 1.9  2003/06/03 20:19:42  tcsmith
 // Modified the close() method to set "closed = true;".
 //
@@ -29,12 +32,14 @@
 #include <log4cplus/streams.h>
 #include <log4cplus/helpers/loglog.h>
 #include <log4cplus/helpers/stringhelper.h>
+#include <log4cplus/helpers/timehelper.h>
 #include <log4cplus/spi/loggingevent.h>
 #include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
 
 using namespace std;
+using namespace log4cplus;
 using namespace log4cplus::helpers;
 
 #define MINIMUM_ROLLING_LOG_SIZE 200*1024L
@@ -45,14 +50,14 @@ using namespace log4cplus::helpers;
 ///////////////////////////////////////////////////////////////////////////////
 
 log4cplus::FileAppender::FileAppender(const log4cplus::tstring& filename, 
-                                      std::ios::openmode mode)
+                                      ios::open_mode mode)
 {
     init(filename, mode);
 }
 
 
-log4cplus::FileAppender::FileAppender(const Properties properties,
-                                      std::ios::openmode mode)
+log4cplus::FileAppender::FileAppender(const Properties& properties,
+                                      ios::open_mode mode)
 : Appender(properties)
 {
      tstring filename = properties.getProperty( LOG4CPLUS_TEXT("File") );
@@ -68,7 +73,7 @@ log4cplus::FileAppender::FileAppender(const Properties properties,
 
 void
 log4cplus::FileAppender::init(const log4cplus::tstring& filename,
-                              std::ios::openmode mode)
+                              ios::open_mode mode)
 {
     this->filename = filename;
     out.open(LOG4CPLUS_TSTRING_TO_STRING(filename).c_str(), mode);
@@ -83,15 +88,15 @@ log4cplus::FileAppender::init(const log4cplus::tstring& filename,
     int columns = 80;
     log4cplus::tstring commentline;
     commentline.resize(columns);
-    std::fill_n(commentline.begin(), columns, LOG4CPLUS_TEXT('#'));
+    fill_n(commentline.begin(), columns, LOG4CPLUS_TEXT('#'));
     
-    out << commentline << std::endl
-        << LOG4CPLUS_TEXT("#") << std::endl
+    out << commentline << endl
+        << LOG4CPLUS_TEXT("#") << endl
         << LOG4CPLUS_TEXT("# Created Appender on: ")
-        << getFormattedTime(time(0), LOG4CPLUS_TEXT("%m-%d-%y %H:%M:%S")) 
-        << std::endl
-        << LOG4CPLUS_TEXT("#") << std::endl
-        << commentline << std::endl;
+        << Time::gettimeofday().getFormattedTime(LOG4CPLUS_TEXT("%m-%d-%y %H:%M:%S")) 
+        << endl
+        << LOG4CPLUS_TEXT("#") << endl
+        << commentline << endl;
 }
 
 
@@ -146,21 +151,21 @@ log4cplus::FileAppender::append(const spi::InternalLoggingEvent& event)
 log4cplus::RollingFileAppender::RollingFileAppender(const log4cplus::tstring& filename,
                                                     long maxFileSize,
                                                     int maxBackupIndex)
-: FileAppender(filename, std::ios::app)
+: FileAppender(filename, ios::app)
 {
     init(maxFileSize, maxBackupIndex);
 }
 
 
 
-log4cplus::RollingFileAppender::RollingFileAppender(const Properties properties)
-: FileAppender(properties, std::ios::app)
+log4cplus::RollingFileAppender::RollingFileAppender(const Properties& properties)
+: FileAppender(properties, ios::app)
 {
     int maxFileSize = 10*1024*1024;
     int maxBackupIndex = 1;
     if(properties.exists( LOG4CPLUS_TEXT("MaxFileSize") )) {
         tstring tmp = properties.getProperty( LOG4CPLUS_TEXT("MaxFileSize") );
-        tmp = toupper(tmp);
+        tmp = toUpper(tmp);
         maxFileSize = atoi(LOG4CPLUS_TSTRING_TO_STRING(tmp).c_str());
         if(tmp.find( LOG4CPLUS_TEXT("MB") ) == (tmp.length() - 2)) {
             maxFileSize *= (1024 * 1024); // convert to megabytes
@@ -264,7 +269,7 @@ log4cplus::RollingFileAppender::rollover()
 
         // Open a new file
         out.open(LOG4CPLUS_TSTRING_TO_STRING(filename).c_str(), 
-                 std::ios::out | std::ios::trunc);
+                 ios::out | ios::trunc);
     }
     else {
         getLogLog().debug(  filename 
@@ -276,8 +281,249 @@ log4cplus::RollingFileAppender::rollover()
 
         // Open it up again in truncation mode
         out.open(LOG4CPLUS_TSTRING_TO_STRING(filename).c_str(), 
-                 std::ios::out | std::ios::trunc);
+                 ios::out | ios::trunc);
     }
 }
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// log4cplus::DailyRollingFileAppender ctors and dtor
+///////////////////////////////////////////////////////////////////////////////
+
+DailyRollingFileAppender::DailyRollingFileAppender(const log4cplus::tstring& filename,
+                                                   DailyRollingFileSchedule schedule)
+: FileAppender(filename, ios::app)
+{
+    init(schedule);
+}
+
+
+
+DailyRollingFileAppender::DailyRollingFileAppender(const Properties& properties)
+: FileAppender(properties)
+{
+    DailyRollingFileSchedule theSchedule = DAILY;
+    tstring scheduleStr = properties.getProperty(LOG4CPLUS_TEXT("Schedule"));\
+    scheduleStr = toUpper(scheduleStr);
+
+    if(scheduleStr == LOG4CPLUS_TEXT("MONTHLY")) {
+        theSchedule = MONTHLY;
+    }
+    else if(scheduleStr == LOG4CPLUS_TEXT("WEEKLY")) {
+        theSchedule = WEEKLY;
+    }
+    else if(scheduleStr == LOG4CPLUS_TEXT("DAILY")) {
+        theSchedule = DAILY;
+    }
+    else if(scheduleStr == LOG4CPLUS_TEXT("TWICE_DAILY")) {
+        theSchedule = TWICE_DAILY;
+    }
+    else if(scheduleStr == LOG4CPLUS_TEXT("HOURLY")) {
+        theSchedule = HOURLY;
+    }
+    else if(scheduleStr == LOG4CPLUS_TEXT("MINUTELY")) {
+        theSchedule = MINUTELY;
+    }
+    else {
+        getLogLog().warn(  "DailyRollingFileAppender::ctor()- \"Schedule\" not valid: "
+			 + properties.getProperty(LOG4CPLUS_TEXT("Schedule")));
+    }
+
+    init(theSchedule);
+}
+
+
+
+void
+DailyRollingFileAppender::init(DailyRollingFileSchedule schedule)
+{
+    this->schedule = schedule;
+
+    Time now = Time::gettimeofday();
+    now.usec(0);
+    struct tm time;
+    now.localtime(&time);
+    time.tm_sec = 0;
+
+    switch(schedule) {
+    case MONTHLY:
+        time.tm_mday = 1;
+        time.tm_hour = 0;
+        time.tm_min = 0;
+        break;
+
+    case WEEKLY:
+        time.tm_mday += (7 - time.tm_wday);
+        time.tm_hour = 0;
+        time.tm_min = 0;
+        break;
+
+    case DAILY:
+        time.tm_hour = 0;
+        time.tm_min = 0;
+        break;
+
+    case TWICE_DAILY:
+        if(time.tm_hour >= 12) {
+            time.tm_hour = 12;
+        }
+        else {
+            time.tm_hour = 0;
+        }
+        time.tm_min = 0;
+        break;
+
+    case HOURLY:
+        time.tm_min = 0;
+        break;
+
+    case MINUTELY:
+        break;
+    };
+    now.setTime(&time);
+
+    scheduledFilename = getFilename(now);
+    nextRolloverTime = calculateNextRolloverTime(now);
+}
+
+
+
+DailyRollingFileAppender::~DailyRollingFileAppender()
+{
+    destructorImpl();
+}
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// log4cplus::DailyRollingFileAppender protected methods
+///////////////////////////////////////////////////////////////////////////////
+
+// This method does not need to be locked since it is called by
+// doAppend() which performs the locking
+void
+DailyRollingFileAppender::append(const spi::InternalLoggingEvent& event)
+{
+    if(!out.is_open()) {
+        getErrorHandler()->error(  LOG4CPLUS_TEXT("file is not open: ") 
+                                 + filename);
+        return;
+    }
+
+    if(event.timestamp >= nextRolloverTime) {
+        rollover();
+    }
+
+    layout->formatAndAppend(out, event);
+}
+
+
+
+void
+DailyRollingFileAppender::rollover()
+{
+    // Close the current file
+    out.close();
+    out.clear(); // reset flags since the C++ standard specified that all the
+                 // flags should remain unchanged on a close
+
+    // Rename fileName to fileName.1
+    getLogLog().debug(  LOG4CPLUS_TEXT("Renaming file ") + filename 
+                      + LOG4CPLUS_TEXT(" to ") + scheduledFilename);
+    rename(LOG4CPLUS_TSTRING_TO_STRING(filename).c_str(), 
+           LOG4CPLUS_TSTRING_TO_STRING(scheduledFilename).c_str());
+
+    // Open a new file
+    out.open(LOG4CPLUS_TSTRING_TO_STRING(filename).c_str(), 
+             ios::out | ios::trunc);
+
+    // Calculate the next rollover time
+    scheduledFilename = getFilename(nextRolloverTime);
+    nextRolloverTime = calculateNextRolloverTime(nextRolloverTime);
+}
+
+
+
+log4cplus::helpers::Time
+DailyRollingFileAppender::calculateNextRolloverTime(const log4cplus::helpers::Time& t)
+{
+    switch(schedule) {
+    case MONTHLY: {
+            struct tm nextMonthTime;
+	    t.localtime(&nextMonthTime);
+            nextMonthTime.tm_mon += 1;
+
+            Time ret;
+            if(ret.setTime(&nextMonthTime) == -1) {
+                getLogLog().error(LOG4CPLUS_TEXT("DailyRollingFileAppender::calculateNextRolloverTime()- setTime() returned error"));
+                ret = (t + Time(2678400));
+            }
+
+            return ret;
+        }
+        break;
+
+    case WEEKLY:
+        return (t + Time(604800)); // 7 * 24 * 60 * 60 seconds
+        break;
+
+    case DAILY:
+        return (t + Time(86400)); //      24 * 60 * 60 seconds
+        break;
+
+    case TWICE_DAILY:
+        return (t + Time(43200)); //      12 * 60 * 60 seconds
+        break;
+
+    case HOURLY:
+        return (t + Time(3600));  //           60 * 60 seconds
+        break;
+
+    case MINUTELY:
+        return (t + Time(60));    //                60 seconds
+    };
+
+    getLogLog().error(LOG4CPLUS_TEXT("DailyRollingFileAppender::calculateNextRolloverTime()- invalid schedule value"));
+    return (t + Time(86400));
+}
+
+
+
+log4cplus::tstring
+DailyRollingFileAppender::getFilename(const log4cplus::helpers::Time& t)
+{
+    tstring pattern;
+    switch(schedule) {
+    case MONTHLY:
+        pattern = "%Y-%m";
+        break;
+
+    case WEEKLY:
+        pattern = "%Y-%W";
+        break;
+
+    case DAILY:
+        pattern = "%Y-%m-%d";
+        break;
+
+    case TWICE_DAILY:
+        pattern = "%Y-%m-%d-%p";
+        break;
+
+    case HOURLY:
+        pattern = "%Y-%m-%d-%H";
+        break;
+
+    case MINUTELY:
+        pattern = "%Y-%m-%d-%H-%M";
+        break;
+    };
+
+    return filename + LOG4CPLUS_TEXT(".") + t.getFormattedTime(pattern, false);
+}
+
+
 
 
