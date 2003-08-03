14aef10bb1949fd7caad1c042088228d217ce99e
tcsmith <>
2003-08-03 17:09:14 +0000

    Initial version.

---
+++ tests/custom_loglevel/customloglevel.cxx
@@ -0,0 +1,40 @@
+
+#include "customloglevel.h"
+
+#define _CRITICAL_STRING "CRITICAL"
+
+
+
+tstring
+criticalToStringMethod(LogLevel ll)
+{
+    if(ll == CRITICAL_LOG_LEVEL) {
+        return _CRITICAL_STRING;
+    }
+    else {
+        return tstring();
+    }
+}
+
+
+
+LogLevel
+criticalFromStringMethod(const tstring& s) 
+{
+    if(s == _CRITICAL_STRING) return CRITICAL_LOG_LEVEL;
+
+    return NOT_SET_LOG_LEVEL;
+}
+
+
+
+class CriticalLogLevelInitializer {
+public:
+    CriticalLogLevelInitializer() {
+        getLogLevelManager().pushToStringMethod(criticalToStringMethod);
+        getLogLevelManager().pushFromStringMethod(criticalFromStringMethod);
+    }
+};
+
+CriticalLogLevelInitializer criticalLogLevelInitializer_;
+
