5f85ec7b3e5a8512ec3e75092e158f29d180af57
wilx <>
2008-12-08 09:58:12 +0000

    Use time_t type to store seconds in helpers::Time.

--- log4cplus/include/log4cplus/helpers/timehelper.h
+++ log4cplus/include/log4cplus/helpers/timehelper.h
@@ -27,103 +27,98 @@
 
 
 namespace log4cplus {
-    namespace helpers {
 
-        /**
-         * This class represents a Epoch time with microsecond accuracy.
-         */
-        class LOG4CPLUS_EXPORT Time {
-        public:
-            Time();
-            Time(long tv_sec, long tv_usec);
-            Time(time_t time);
+namespace helpers {
 
-            /**
-             * Returns the current time using the <code>gettimeofday()</code>
-             * method if it is available on the current platform.  (Not on 
-             * WIN32.)
-             */
-            static Time gettimeofday();
+/**
+ * This class represents a Epoch time with microsecond accuracy.
+ */
+class LOG4CPLUS_EXPORT Time {
+public:
+    Time();
+    Time(time_t tv_sec, long tv_usec);
+    explicit Time(time_t time);
 
-          // Methods
-            /**
-             * Returns <i>seconds</i> value.
-             */
-            long sec() const { return tv_sec; }
+    /**
+     * Returns the current time using the <code>gettimeofday()</code>
+     * method if it is available on the current platform.  (Not on 
+     * WIN32.)
+     */
+    static Time gettimeofday();
 
-            /**
-             * Returns <i>microseconds</i> value.
-             */
-            long usec() const { return tv_usec; }
+  // Methods
+    /**
+     * Returns <i>seconds</i> value.
+     */
+    time_t sec() const { return tv_sec; }
 
-            /**
-             * Sets the <i>seconds</i> value.
-             */
-            void sec(long s) { tv_sec = s; }
+    /**
+     * Returns <i>microseconds</i> value.
+     */
+    long usec() const { return tv_usec; }
 
-            /**
-             * Sets the <i>microseconds</i> value.
-             */
-            void usec(long us) { tv_usec = us; }
+    /**
+     * Sets the <i>seconds</i> value.
+     */
+    void sec(time_t s) { tv_sec = s; }
 
-            /**
-             * Sets this Time using the <code>mktime</code> function.
-             */
-            int setTime(struct tm* t);
+    /**
+     * Sets the <i>microseconds</i> value.
+     */
+    void usec(long us) { tv_usec = us; }
 
-            /**
-             * Returns this Time as a <code>time_t></code> value.
-             */
-            time_t getTime() const;
+    /**
+     * Sets this Time using the <code>mktime</code> function.
+     */
+    time_t setTime(struct tm* t);
 
-            /**
-             * Populates <code>tm</code> using the <code>gmtime()</code>
-             * function.
-             */
-            void gmtime(struct tm* t) const;
+    /**
+     * Returns this Time as a <code>time_t></code> value.
+     */
+    time_t getTime() const;
 
-            /**
-             * Populates <code>tm</code> using the <code>localtime()</code>
-             * function.
-             */
-            void localtime(struct tm* t) const;
+    /**
+     * Populates <code>tm</code> using the <code>gmtime()</code>
+     * function.
+     */
+    void gmtime(struct tm* t) const;
 
-            /**
-             * Returns a string with a "formatted time" specified by
-             * <code>fmt</code>.  It used the <code>strftime()</code>
-             * function to do this.  
-             * <p>
-             * Look at your platform's <code>strftime()</code> documentation
-             * for the formatting options available.
-             * <p>
-             * The following additional options are provided:<br>
-             * <code>%q</code> - 3 character field that provides milliseconds
-             * <code>%Q</code> - 7 character field that provides fractional 
-             * milliseconds.
-             */
-            log4cplus::tstring getFormattedTime(const log4cplus::tstring& fmt,
-                                                bool use_gmtime = false) const;
+    /**
+     * Populates <code>tm</code> using the <code>localtime()</code>
+     * function.
+     */
+    void localtime(struct tm* t) const;
 
-          // Operators
-            Time& operator+=(const Time& rhs);
-            Time& operator-=(const Time& rhs);
-            Time& operator/=(long rhs);
-            Time& operator*=(long rhs);
-	    bool operator==(const Time& rhs) { return tv_sec == rhs.tv_sec &&
-		                                      tv_usec == rhs.tv_usec; }
-	    bool operator!=(const Time& rhs) { return !(*this == rhs); }
+    /**
+     * Returns a string with a "formatted time" specified by
+     * <code>fmt</code>.  It used the <code>strftime()</code>
+     * function to do this.  
+     * <p>
+     * Look at your platform's <code>strftime()</code> documentation
+     * for the formatting options available.
+     * <p>
+     * The following additional options are provided:<br>
+     * <code>%q</code> - 3 character field that provides milliseconds
+     * <code>%Q</code> - 7 character field that provides fractional 
+     * milliseconds.
+     */
+    log4cplus::tstring getFormattedTime(const log4cplus::tstring& fmt,
+                                        bool use_gmtime = false) const;
 
-        private:
-            void build_q_value (log4cplus::tstring & q_str) const;
-            void build_uc_q_value (log4cplus::tstring & uc_q_str) const;
+  // Operators
+    Time& operator+=(const Time& rhs);
+    Time& operator-=(const Time& rhs);
+    Time& operator/=(long rhs);
+    Time& operator*=(long rhs);
 
-          // Data
-            long  tv_sec;   /* seconds */
-            long  tv_usec;  /* microseconds */
-        };
+private:
+    void build_q_value (log4cplus::tstring & q_str) const;
+    void build_uc_q_value (log4cplus::tstring & uc_q_str) const;
 
-    }
-}
+  // Data
+    time_t tv_sec;  /* seconds */
+    long tv_usec;  /* microseconds */
+};
 
 
 LOG4CPLUS_EXPORT const log4cplus::helpers::Time operator+
@@ -153,6 +148,11 @@ LOG4CPLUS_EXPORT bool operator==(const log4cplus::helpers::Time& lhs,
                                  const log4cplus::helpers::Time& rhs);
 LOG4CPLUS_EXPORT bool operator!=(const log4cplus::helpers::Time& lhs,
                                  const log4cplus::helpers::Time& rhs);
+
+} // namespace helpers
+
+} // namespace log4cplus
+
 
 #endif // _LOG4CPLUS_HELPERS_TIME_HELPER_HEADER_
 
