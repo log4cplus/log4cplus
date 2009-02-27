544ec1ff2f68d0c95233c5f90b5f7bbc6a1a23c3
juanalday <>
2009-02-27 14:49:32 +0000

    Indented the code to adjust to the 80 column rule

--- src/property.cxx
+++ src/property.cxx
@@ -29,182 +29,181 @@
 namespace log4cplus { namespace helpers {
 
 
-		namespace
-		{
+namespace {
 
-			static const tchar PROPERTIES_COMMENT_CHAR = LOG4CPLUS_TEXT('#');
+static const tchar PROPERTIES_COMMENT_CHAR = LOG4CPLUS_TEXT('#');
 
-			struct IsNotSpace 
-			{
+struct IsNotSpace 
+{
 #if defined (UNICODE)
-				bool operator()(int c) const { return (std::iswspace(c) ? false : true); }
+	bool operator()(int c) const { return (std::iswspace(c) ? false : true); }
 #else
-				bool operator()(int c) const { return (std::isspace(c) ? false : true); }
+	bool operator()(int c) const { return (std::isspace(c) ? false : true); }
 #endif
-			};
+};
+	
+/**
+ * Removes all leading spaces from the input string
+ */
+tstring&
+trim_left (tstring& input)
+{
+	input.erase(input.begin(), 
+				std::find_if(input.begin(), input.end(), IsNotSpace()));
+	return input;
+}
 
-			/**
-			 * Removes all leading spaces from the input string
-			 */
-			tstring&
-			trim_left (tstring& input)
-			{
-				input.erase(input.begin(), std::find_if(input.begin(), input.end(), IsNotSpace()));
-				return input;
-			}
-
-			tstring
-			trim_left_copy(const tstring& input)
-			{
-				return tstring(std::find_if(input.begin(), input.end(), IsNotSpace()), input.end());
-			}
-
-
-			tstring&
-			trim_right(tstring& input)
-			{
-				input.erase(std::find_if(input.rbegin(), input.rend(), IsNotSpace()).base(), input.end());
-				return input;
-			}
-
-			tstring
-			trim_right_copy(const tstring& input)
-			{
-				return tstring(input.begin(), std::find_if(input.rbegin(), input.rend(), IsNotSpace()).base());
-
-			}
+tstring
+trim_left_copy(const tstring& input)
+{
+	return tstring(std::find_if(input.begin(), input.end(), IsNotSpace()), 
+				   input.end());
+}
 
 
-			static tstring&
-			trim(tstring & input)
-			{
-				return trim_left(trim_right(input));
-			}
+tstring&
+trim_right(tstring& input)
+{
+	input.erase(std::find_if(input.rbegin(), input.rend(), IsNotSpace()).base(),
+				input.end());
+	return input;
+}
 
-			static tstring
-			trim_copy(const tstring& input)
-			{
-				tstring returnValue(input);
-				return trim_left(trim_right(returnValue));
-			}
+tstring
+trim_right_copy(const tstring& input)
+{
+	return tstring(input.begin(), 
+				   std::find_if(input.rbegin(), 
+								input.rend(), 
+								IsNotSpace()).base());
+
+}
 
 
-		} // namespace
+//static tstring& trim(tstring & input) {return trim_left(trim_right(input));}
+
+static tstring
+trim_copy(const tstring& input)
+{
+	tstring returnValue(input);
+	return trim_left(trim_right(returnValue));
+}
+
+
+} // namespace
 
 
 
-		///////////////////////////////////////////////////////////////////////////////
-		// Properties ctors and dtor
-		///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Properties ctors and dtor
+///////////////////////////////////////////////////////////////////////////////
 
-		Properties::Properties() 
-		{
+Properties::Properties() 
+{
+}
+
+
+
+Properties::Properties(log4cplus::tistream& input)
+{
+	init(input);
+}
+
+
+
+Properties::Properties(const log4cplus::tstring& inputFile)
+{
+	if (!inputFile.empty())
+	{
+		tifstream tmp(LOG4CPLUS_TSTRING_TO_STRING(inputFile).c_str());
+		init(tmp);
+	}
+}
+
+
+
+void 
+Properties::init(log4cplus::tistream& input) 
+{
+	tstring buffer;
+	while (!input.fail() && std::getline (input, buffer))
+	{
+		trim_left (buffer);
+		
+		if (!buffer.empty() && (*buffer.begin() != PROPERTIES_COMMENT_CHAR))
+		{		
+			// Check if we have a trailing \r because we are 
+			// reading a properties file produced on Windows.
+			if (*buffer.rend() == LOG4CPLUS_TEXT('\r')) 
+				buffer.resize (buffer.size() - 1);
+			
+			const tstring::size_type idx = buffer.find('=');
+			if (idx != tstring::npos)
+				setProperty(trim_right_copy(buffer.substr(0, idx)),
+								trim_copy(buffer.substr(idx + 1)));
 		}
+	}
+}
 
 
 
-		Properties::Properties(log4cplus::tistream& input)
-		{
-			init(input);
-		}
+Properties::~Properties() 
+{
+}
 
 
 
-		Properties::Properties(const log4cplus::tstring& inputFile)
-		{
-			if (!inputFile.empty())
-			{
-				tifstream tmp(LOG4CPLUS_TSTRING_TO_STRING(inputFile).c_str());
-				init(tmp);
-			}
-		}
+///////////////////////////////////////////////////////////////////////////////
+// helpers::Properties public methods
+///////////////////////////////////////////////////////////////////////////////
+
+const tstring&
+Properties::getProperty(const tstring& key) const 
+{
+	const map_type::const_iterator it (data_map.find(key));
+	return (it == data_map.end() ? log4cplus::internal::empty_str : it->second);
+}
 
 
 
-		void 
-		Properties::init(log4cplus::tistream& input) 
-		{
-			if (input)
-			{
-				tstring buffer;
-				while (std::getline (input, buffer))
-				{
-					trim_left (buffer);
-					
-					if (!buffer.empty() && (*buffer.begin() != PROPERTIES_COMMENT_CHAR) )
-					{
-        
-						// Check if we have a trailing \r because we are 
-						// reading a properties file produced on Windows.
-						if (*buffer.rend() == LOG4CPLUS_TEXT('\r')) // Remove trailing 'Windows' \r.
-							buffer.resize (buffer.size() - 1);
-						
-						const tstring::size_type idx = buffer.find('=');
-						if (idx != tstring::npos)
-							setProperty(trim_right_copy(buffer.substr(0, idx)), trim_copy(buffer.substr(idx + 1)));
-					}
-				}
-			}
-		}
+const tstring&
+Properties::getProperty(const tstring& key, const tstring& defaultVal) const
+{
+	const map_type::const_iterator it = data_map.find (key);
+	return (it == data_map.end() ? defaultVal : it->second);
+}
 
 
-
-		Properties::~Properties() 
-		{
-		}
-
-
-
-		///////////////////////////////////////////////////////////////////////////////
-		// helpers::Properties public methods
-		///////////////////////////////////////////////////////////////////////////////
-
-		const tstring&
-		Properties::getProperty(const tstring& key) const 
-		{
-			const map_type::const_iterator it (data_map.find(key));
-			return (it == data_map.end() ? log4cplus::internal::empty_str : it->second);
-		}
+void
+Properties::setProperty(const log4cplus::tstring& key,
+						const log4cplus::tstring& value)
+{
+	data_map.insert(std::make_pair(key, value));
+	keys.insert(key);			
+}
 
 
-
-		const tstring&
-		Properties::getProperty(const tstring& key, const tstring& defaultVal) const
-		{
-			const map_type::const_iterator it = data_map.find (key);
-			return (it == data_map.end() ? defaultVal : it->second);
-		}
-
-
-		void
-		Properties::setProperty(const log4cplus::tstring& key,
-								const log4cplus::tstring& value)
-		{
-			data_map.insert(std::make_pair(key, value));
-			keys.insert(key);			
-		}
+bool
+Properties::removeProperty(const log4cplus::tstring& key)
+{
+	keys.erase(key);
+	return data_map.erase(key) > 0;
+}
 
 
-		bool
-		Properties::removeProperty(const log4cplus::tstring& key)
-		{
-			keys.erase(key);
-			return data_map.erase(key) > 0;
-		}
+Properties 
+Properties::getPropertySubset(const log4cplus::tstring& prefix) const
+{
+	Properties ret;
+	for (map_type::const_iterator iter = data_map.begin(); 
+		 iter != data_map.end(); ++iter)
+	{
+		if (iter->first.find(prefix) == 0)
+			ret.setProperty(iter->first.substr(prefix.size()), iter->second);
+	}
+
+	return ret;
+}
 
 
-		Properties 
-		Properties::getPropertySubset(const log4cplus::tstring& prefix) const
-		{
-			Properties ret;
-			for (map_type::const_iterator iter = data_map.begin(); iter != data_map.end(); ++iter)
-			{
-				if (iter->first.find(prefix) == 0)
-					ret.setProperty(iter->first.substr(prefix.size()), iter->second);
-			}
-
-			return ret;
-		}
-
-
-	} } // namespace log4cplus { namespace helpers {
+} } // namespace log4cplus { namespace helpers {
