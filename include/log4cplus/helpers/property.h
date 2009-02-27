55860d997d8888130d93f6bd16025f60f038a140
juanalday <>
2009-02-27 00:31:54 +0000

    propertyNames() returns a reference to the list of keys instead of a full copy

--- include/log4cplus/helpers/property.h
+++ include/log4cplus/helpers/property.h
@@ -20,7 +20,7 @@
 #include <log4cplus/streams.h>
 #include <log4cplus/tstring.h>
 #include <map>
-#include <vector>
+#include <set>
 
 
 namespace log4cplus {
@@ -28,37 +28,44 @@ namespace log4cplus {
 
         class LOG4CPLUS_EXPORT Properties {
         public:
+
+			typedef std::map<log4cplus::tstring, log4cplus::tstring> map_type;
+			
             Properties();
             explicit Properties(log4cplus::tistream& input);
             explicit Properties(const log4cplus::tstring& inputFile);
             virtual ~Properties();
 
-          // constants
-            static const tchar PROPERTIES_COMMENT_CHAR;
-
           // methods
             /**
              * Tests to see if <code>key</code> can be found in this map.
              */
-            bool exists(const log4cplus::tstring& key) const {
-                return data.find(key) != data.end();
+            bool exists(const log4cplus::tstring& key) const 
+			{
+                return data_map.find(key) != data_map.end();
             }
 
 
             /**
              * Returns the number of entries in this map.
              */
-            size_t size() const {
-                return data.size();
+            size_t size() const 
+			{
+                return data_map.size();
             }
 
+			/**
+			 * Returns the internal structure containing the properties
+			 * @return the internal <code>map_type</code> structure holding the properties
+			 */
+			const map_type& data() const {return data_map;}
             /**
              * Searches for the property with the specified key in this property
              * list. If the key is not found in this property list, the default
              * property list, and its defaults, recursively, are then checked. 
              * The method returns <code>null</code> if the property is not found.
              */
-            log4cplus::tstring const & getProperty(const log4cplus::tstring& key) const;
+            const log4cplus::tstring& getProperty(const log4cplus::tstring& key) const;
 
             /**
              * Searches for the property with the specified key in this property
@@ -67,13 +74,16 @@ namespace log4cplus {
              * The method returns the default value argument if the property is 
              * not found.
              */
-            log4cplus::tstring getProperty(const log4cplus::tstring& key,
-                                           const log4cplus::tstring& defaultVal) const;
+            const log4cplus::tstring& getProperty(const log4cplus::tstring& key,
+												  const log4cplus::tstring& defaultVal) const;
 
             /**
              * Returns all the keys in this property list.
              */
-            std::vector<log4cplus::tstring> propertyNames() const;
+            const std::set<log4cplus::tstring>& propertyNames() const
+			{
+				return keys;
+			}
 
             /**
              * Inserts <code>value</code> into this map indexed by <code>key</code>.
@@ -92,16 +102,19 @@ namespace log4cplus {
              */
             Properties getPropertySubset(const log4cplus::tstring& prefix) const;
 
+
+
         protected:
           // Types
 //            LOG4CPLUS_EXPIMP_TEMPLATE template class LOG4CPLUS_EXPORT std::map<log4cplus::tstring, log4cplus::tstring>;
-            typedef std::map<log4cplus::tstring, log4cplus::tstring> StringMap;
 
           // Methods
             void init(log4cplus::tistream& input);
 
+		private:
           // Data
-            StringMap data;
+            map_type data_map;
+			std::set<log4cplus::tstring> keys; // All the keys in the map. 
         };
     } // end namespace helpers
 
