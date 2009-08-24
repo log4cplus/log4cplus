e8f6973ffcd566f515dbb71d6563017b16350ddd
wilx <>
2009-08-24 18:28:26 +0000

    loggingserver.cxx: Remove unnecessary includes.

--- loggingserver/loggingserver.cxx
+++ loggingserver/loggingserver.cxx
@@ -13,14 +13,10 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <log4cplus/config.hxx>
 #include <log4cplus/configurator.h>
-#include <log4cplus/consoleappender.h>
 #include <log4cplus/socketappender.h>
-#include <log4cplus/helpers/loglog.h>
 #include <log4cplus/helpers/socket.h>
 #include <log4cplus/helpers/threads.h>
-#include <log4cplus/spi/loggerimpl.h>
 #include <log4cplus/spi/loggingevent.h>
 
 
