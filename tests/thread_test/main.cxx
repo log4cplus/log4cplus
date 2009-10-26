82e05f78641ad475c90ff4f678c320beaed0648c
wilx <>
2009-10-26 09:24:06 +0000

    Merge revision 1001 from PRODUCTION_1_0_x branch.
    r1001:
    
    main.cxx: Fix thread_test compilation on AIX. Use log4cplus::thread::yield() instead of
    just thread::yield() to avoid ambiguity.

--- tests/thread_test/main.cxx
+++ tests/thread_test/main.cxx
@@ -35,7 +35,7 @@ public:
             sleep(0, 75 * MILLIS_TO_NANOS);
             LOG4CPLUS_INFO(logger, LOG4CPLUS_TEXT("Actually doing something...DONE"));
         LOG4CPLUS_END_SYNCHRONIZE_ON_MUTEX;
-        thread::yield();
+        log4cplus::thread::yield();
     }
 
     ~SlowObject ()
