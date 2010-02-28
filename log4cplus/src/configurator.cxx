05d98ab770352b0b04a2acbc4d2f578a04142e5a
wilx <>
2010-02-28 08:29:05 +0000

    configurator.cxx: Fix bug #2955058. In replaceEnvironVariables(), properly loop over properties keys if any property has changed. Make the looping conditional on the fRecursiveExpansion flag.

--- log4cplus/src/configurator.cxx
+++ log4cplus/src/configurator.cxx
@@ -279,17 +279,22 @@ PropertyConfigurator::reconfigure()
 void
 PropertyConfigurator::replaceEnvironVariables()
 {
-    std::vector<tstring> keys = properties.propertyNames();
-    std::vector<tstring>::const_iterator it = keys.begin();
     tstring val, subKey, subVal;
+    std::vector<tstring> keys;
+    bool const rec_exp
+        = !! (flags & PropertyConfigurator::fRecursiveExpansion);
     bool changed;
+
     do 
     {
         changed = false;
-        for(; it != keys.end(); ++it)
+        properties.propertyNames().swap (keys);
+        for (std::vector<tstring>::const_iterator it = keys.begin();
+            it != keys.end(); ++it)
         {
             tstring const & key = *it;
             val = properties.getProperty(key);
+
             subKey.clear ();
             if (substVars(subKey, key, properties, getLogLog(), flags))
             {
@@ -297,7 +302,7 @@ PropertyConfigurator::replaceEnvironVariables()
                 properties.setProperty(subKey, val);
                 changed = true;
             }
-            
+
             subVal.clear ();
             if (substVars(subVal, val, properties, getLogLog(), flags))
             {
@@ -306,7 +311,7 @@ PropertyConfigurator::replaceEnvironVariables()
             }
         }
     }
-    while (changed);
+    while (changed && rec_exp);
 }
 
 
