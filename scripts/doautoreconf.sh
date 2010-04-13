9911cd4ba1bcd67c9f95698f6f1721902ce535fc
wilx <>
2010-04-13 07:26:05 +0000

    doautoreconf.sh: New helper script.

---
+++ scripts/doautoreconf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export AUTOMAKE_SUFFIX=-1.10.1
+export AUTOCONF_SUFFIX=-2.62
+export LIBTOOL_SUFFIX=-2.2.6
+
+export ACLOCAL="aclocal${AUTOMAKE_SUFFIX}"
+export AUTOMAKE="automake${AUTOMAKE_SUFFIX}"
+
+export AUTOCONF="autoconf${AUTOCONF_SUFFIX}"
+export AUTOHEADER="autoheader${AUTOCONF_SUFFIX}"
+export AUTOM4TE="autom4te${AUTOCONF_SUFFIX}"
+export AUTORECONF="autoreconf${AUTOCONF_SUFFIX}"
+export AUTOSCAN="autoscan${AUTOCONF_SUFFIX}"
+export AUTOUPDATE="autoupdate${AUTOCONF_SUFFIX}"
+export IFNAMES="ifnames${AUTOCONF_SUFFIX}"
+
+export LIBTOOLIZE="libtoolize${LIBTOOL_SUFFIX}"
+export LIBTOOL="libtool${LIBTOOL_SUFFIX}"
+
+$AUTORECONF -f -i
