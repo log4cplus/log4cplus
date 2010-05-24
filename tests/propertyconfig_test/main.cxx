81e3e76d3f2e32defd9eda1c213df3b01b6a4424
wilx <>
2010-05-24 15:19:35 +0000

    main.cxx: Wrap string literals to LOG4CPLUS_TEXT().

--- tests/propertyconfig_test/main.cxx
+++ tests/propertyconfig_test/main.cxx
@@ -1,9 +1,9 @@
-
-#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
 #include <log4cplus/configurator.h>
 #include <log4cplus/helpers/loglog.h>
 #include <log4cplus/helpers/stringhelper.h>
 #include <log4cplus/loggingmacros.h>
+
 
 using namespace std;
 using namespace log4cplus;
@@ -14,26 +14,26 @@ using namespace log4cplus::helpers;
 int
 main()
 {
-    tcout << "Entering main()..." << endl;
+    tcout << LOG4CPLUS_TEXT("Entering main()...") << endl;
     LogLog::getLogLog()->setInternalDebugging(true);
     Logger root = Logger::getRoot();
     try {
         PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("log4cplus.properties"));
         Logger fileCat = Logger::getInstance(LOG4CPLUS_TEXT("filelogger"));
 
-	LOG4CPLUS_WARN(root, "Testing....");
+	LOG4CPLUS_WARN(root, LOG4CPLUS_TEXT("Testing...."));
 
-	LOG4CPLUS_WARN(root, "Writing messages to log....");
+	LOG4CPLUS_WARN(root, LOG4CPLUS_TEXT("Writing messages to log...."));
         for(int i=0; i<10000; ++i) {
-            LOG4CPLUS_WARN(fileCat, "This is a WARNING..." << i);
+	    LOG4CPLUS_WARN(fileCat, LOG4CPLUS_TEXT("This is a WARNING...") << i);
         }
     }
     catch(...) {
-        tcout << "Exception..." << endl;
-	LOG4CPLUS_FATAL(root, "Exception occured...");
+        tcout << LOG4CPLUS_TEXT("Exception...") << endl;
+	LOG4CPLUS_FATAL(root, LOG4CPLUS_TEXT("Exception occured..."));
     }
 
-    tcout << "Exiting main()..." << endl;
+    tcout << LOG4CPLUS_TEXT("Exiting main()...") << endl;
     return 0;
 }
 
