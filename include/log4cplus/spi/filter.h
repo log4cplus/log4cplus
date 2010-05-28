659175b4ab21940ba3627e0e13fede818d104593
wilx <>
2010-05-28 09:06:51 +0000

    *: Update year in copyright header.
    loggingserver.cxx: Change license header to ASL 2.0 license header.

--- include/log4cplus/spi/filter.h
+++ include/log4cplus/spi/filter.h
@@ -4,7 +4,7 @@
 // Author:  Tad E. Smith
 //
 //
-// Copyright 1999-2009 Tad E. Smith
+// Copyright 1999-2010 Tad E. Smith
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
