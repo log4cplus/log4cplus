ced381455e9e482a10a6e96f8cabe77b55a5f393
Vaclav Zeman <vhaisman@gmail.com>
2012-02-24 10:01:09 +0100

    syncprims-pthreads.h: Avoid function-try-catch blocks in dtors.

--- include/log4cplus/helpers/syncprims-pthreads.h
+++ include/log4cplus/helpers/syncprims-pthreads.h
@@ -53,14 +53,16 @@ struct PthreadMutexAttr
 
 
     ~PthreadMutexAttr ()
-    try
     {
-        int ret = pthread_mutexattr_destroy (&attr);
-        if (ret != 0)
-            LOG4CPLUS_THROW_RTE ("PthreadMutexAttr::~PthreadMutexAttr");
+        try
+        {
+            int ret = pthread_mutexattr_destroy (&attr);
+            if (ret != 0)
+                LOG4CPLUS_THROW_RTE ("PthreadMutexAttr::~PthreadMutexAttr");
+        }
+        catch (...)
+        { }
     }
-    catch (...)
-    { }
 
 
     void
@@ -106,9 +108,14 @@ Mutex::Mutex (Mutex::Type t)
 inline
 Mutex::~Mutex ()
 {
-    int ret = pthread_mutex_destroy (&mtx);
-    if (ret != 0)
-        LOG4CPLUS_THROW_RTE ("Mutex::~Mutex");
+    try
+    {
+        int ret = pthread_mutex_destroy (&mtx);
+        if (ret != 0)
+            LOG4CPLUS_THROW_RTE ("Mutex::~Mutex");
+    }
+    catch (...)
+    { }
 }
 
 
@@ -157,14 +164,16 @@ Semaphore::Semaphore (unsigned max, unsigned initial)
 
 inline
 Semaphore::~Semaphore ()
-try
 {
-    int ret = sem_destroy (&sem);
-    if (ret != 0)
-        LOG4CPLUS_THROW_RTE ("Semaphore::~Semaphore");
+    try
+    {
+        int ret = sem_destroy (&sem);
+        if (ret != 0)
+            LOG4CPLUS_THROW_RTE ("Semaphore::~Semaphore");
+    }
+    catch (...)
+    { }
 }
-catch (...)
-{ }
 
 
 inline
@@ -205,14 +214,16 @@ ManualResetEvent::ManualResetEvent (bool sig)
 
 inline
 ManualResetEvent::~ManualResetEvent ()
-try
 {
-    int ret = pthread_cond_destroy (&cv);
-    if (ret != 0)
-        LOG4CPLUS_THROW_RTE ("ManualResetEvent::~ManualResetEvent");
+    try
+    {
+        int ret = pthread_cond_destroy (&cv);
+        if (ret != 0)
+            LOG4CPLUS_THROW_RTE ("ManualResetEvent::~ManualResetEvent");
+    }
+    catch (...)
+    { }
 }
-catch (...)
-{ }
 
 
 inline
