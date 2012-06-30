28561fa6ebc2e3c30bdcdd7e28ae44c694e84c26
Vaclav Zeman <wilx@users.sourceforge.net>
2012-06-30 07:22:59 +0200

    tests/fileappender_test/main.cxx: Revert unintended change from last commit.

--- tests/fileappender_test/main.cxx
+++ tests/fileappender_test/main.cxx
@@ -3,7 +3,6 @@
 #include <log4cplus/layout.h>
 #include <log4cplus/ndc.h>
 #include <log4cplus/helpers/loglog.h>
-#include <locale>
 
 
 using namespace log4cplus;
@@ -14,12 +13,6 @@ const int LOOP_COUNT = 20000;
 int
 main()
 {
-    std::locale::global (
-        std::locale (
-            std::locale (),
-            new std::codecvt_byname<wchar_t, char, std::mbstate_t>("")));
-            //new std::codecvt<wchar_t, char, std::mbstate_t>()));
-
     helpers::LogLog::getLogLog()->setInternalDebugging(true);
     SharedAppenderPtr append_1(
         new RollingFileAppender(LOG4CPLUS_TEXT("Test.log"), 5*1024, 5));
@@ -33,7 +26,7 @@ main()
 
     for(int i=0; i<LOOP_COUNT; ++i) {
         NDCContextCreator _context(LOG4CPLUS_TEXT("loop"));
-        LOG4CPLUS_DEBUG(subTest, "Entering loop číslo/番：" << i);
+        LOG4CPLUS_DEBUG(subTest, "Entering loop #" << i);
     }
 
 
