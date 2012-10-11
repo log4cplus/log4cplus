904cae953389c6af0a9569327156580749617880
Vaclav Zeman <wilx@users.sourceforge.net>
2012-10-11 21:26:17 +0200

    log4judpappender.h: Fix documentation.

--- include/log4cplus/log4judpappender.h
+++ include/log4cplus/log4judpappender.h
@@ -36,13 +36,10 @@ namespace log4cplus {
      * The Log4jUdpAppender has the following properties:
      *
      * <ul>
-     *
      *   <li>Remote logging is non-intrusive as far as the log event
      *   is concerned. In other words, the event will be logged with
      *   the same time stamp, NDC, location info as if it were logged
      *   locally by the client.</li>
-     *
-     *   <li>Log4jUdpAppender do not use a layout.</li>
      *
      *   <li>Remote logging uses the UDP protocol.</li>
      * </ul>
