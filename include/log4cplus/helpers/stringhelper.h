07bd5cb078a979d5e1bbd07bcc098ab2c8a2c81b
Vaclav Zeman <wilx@users.sourceforge.net>
2013-03-28 23:01:41 +0100

    stringhelper.h: Fix two -Wconversion warnings.

--- include/log4cplus/helpers/stringhelper.h
+++ include/log4cplus/helpers/stringhelper.h
@@ -116,7 +116,7 @@ namespace log4cplus {
                     intType const mod = -(a + value);
                     value = -r;
 
-                    *(it - 1) = LOG4CPLUS_TEXT('0') + static_cast<tchar>(mod);
+                    *(it - 1) = static_cast<tchar>(LOG4CPLUS_TEXT('0') + mod);
                     --it;
                 }
                 else
@@ -180,7 +180,7 @@ namespace log4cplus {
             {
                 intType mod = value % 10;
                 value = value / 10;
-                *(it - 1) = LOG4CPLUS_TEXT('0') + static_cast<tchar>(mod);
+                *(it - 1) = static_cast<tchar>(LOG4CPLUS_TEXT('0') + mod);
             }
 
             if (negative)
