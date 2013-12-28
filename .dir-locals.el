f23647ddba2438bf435bcb9393c64a13b913a6ee
Vaclav Zeman <wilx@users.sourceforge.net>
2013-12-28 21:35:33 +0100

    .dir-locals.el: New file. Emacs settings relevant to log4cplus source formatting.

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (fill-column . 79)
+  (indent-tabs-mode))
+ (c++-mode
+  (show-trailing-whitespace . t)))
