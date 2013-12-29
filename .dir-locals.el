0e1b23f50f54706c2b9d4c110a1cb8589e3ee1e4
Vaclav Zeman <wilx@users.sourceforge.net>
2013-12-29 22:31:27 +0100

    .dir-locals.el: Tweak.

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,9 +5,11 @@
   (fill-column . 79)
   (indent-tabs-mode)
   (show-trailing-whitespace . t)
-  (whitespace-style . (face trailing lines-tail space-before-tab
-                    indentation empty))
+  (whitespace-style face trailing lines-tail space-before-tab indentation empty)
   (whitespace-newline . t))
  (c++-mode
   (tab-width . 4)
-  (whitespace-action . (warn-read-only auto-cleanup))))
+  (whitespace-action warn-read-only auto-cleanup))
+ (cmake-mode
+  (whitespace-action warn-read-only auto-cleanup)))
+
