6529aec80bde96bef663d1ed8a6a4cf81c5a488a
Vaclav Zeman <wilx@users.sourceforge.net>
2015-01-26 16:51:20 +0100

    Cover NDC by unit tests.

--- src/ndc.cxx
+++ src/ndc.cxx
@@ -23,6 +23,10 @@
 #include <utility>
 #include <algorithm>
 
+#if defined (LOG4CPLUS_WITH_UNIT_TESTS)
+#include <catch.hpp>
+#endif
+
 
 namespace log4cplus
 {
@@ -296,5 +300,60 @@ NDCContextCreator::~NDCContextCreator()
     getNDC().pop_void();
 }
 
+
+#if defined (LOG4CPLUS_WITH_UNIT_TESTS)
+CATCH_TEST_CASE ("NDC", "[NDC]")
+{
+    NDC & ndc = getNDC ();
+    static tchar const CONTEXT1[] = LOG4CPLUS_TEXT ("c1");
+    static tchar const CONTEXT2[] = LOG4CPLUS_TEXT ("c2");
+    static tchar const CONTEXT3[] = LOG4CPLUS_TEXT ("c3");
+    static tstring const C1C2 = tstring (CONTEXT1)
+        + LOG4CPLUS_TEXT (' ')
+        + CONTEXT2;
+    static tstring const C1C2C3 = C1C2
+        + LOG4CPLUS_TEXT (' ')
+        + CONTEXT3;
+
+    CATCH_SECTION ("basic")
+    {
+        CATCH_REQUIRE (ndc.get ().empty ());
+        CATCH_REQUIRE (ndc.peek ().empty ());
+        CATCH_REQUIRE (ndc.getDepth () == 0);
+        NDCContextCreator c1 (CONTEXT1);
+        CATCH_REQUIRE (ndc.peek () == CONTEXT1);
+        CATCH_REQUIRE (ndc.get () == CONTEXT1);
+        CATCH_REQUIRE (ndc.getDepth () == 1);
+        {
+            NDCContextCreator c2 (LOG4CPLUS_C_STR_TO_TSTRING (CONTEXT2));
+            CATCH_REQUIRE (ndc.get () == C1C2);
+            CATCH_REQUIRE (ndc.getDepth () == 2);
+            CATCH_REQUIRE (ndc.peek () == CONTEXT2);
+
+            ndc.push (CONTEXT3);
+            CATCH_REQUIRE (ndc.get () == C1C2C3);
+            CATCH_REQUIRE (ndc.peek () == CONTEXT3);
+            CATCH_REQUIRE (ndc.pop () == CONTEXT3);
+        }
+        CATCH_REQUIRE (ndc.peek () == CONTEXT1);
+        CATCH_REQUIRE (ndc.get () == CONTEXT1);
+        CATCH_REQUIRE (ndc.getDepth () == 1);
+    }
+
+    CATCH_SECTION ("remove")
+    {
+        ndc.push (CONTEXT1);
+        CATCH_REQUIRE (ndc.peek () == CONTEXT1);
+        CATCH_REQUIRE (ndc.get () == CONTEXT1);
+        CATCH_REQUIRE (ndc.getDepth () == 1);
+
+        ndc.remove ();
+        CATCH_REQUIRE (ndc.get ().empty ());
+        CATCH_REQUIRE (ndc.peek ().empty ());
+        CATCH_REQUIRE (ndc.getDepth () == 0);
+    }
+}
+
+#endif
 
 } // namespace log4cplus
