d63a51f49207e0eb1d1c7c41ea03bcc5cffbf749
Vaclav Haisman <wilx@users.sourceforge.net>
2015-07-06 14:07:44 +0200

    Fix GitHub bug #92.
    Truncate seconds away from next rollover time.
    
    (cherry picked from commit b114cc48f67afb7b526f4a55cf5d7fb120cd7bd7)

--- src/fileappender.cxx
+++ src/fileappender.cxx
@@ -33,6 +33,7 @@
 #include <algorithm>
 #include <sstream>
 #include <cstdio>
+#include <cmath>
 #include <stdexcept>
 
 #if defined (__BORLANDC__)
@@ -809,11 +810,16 @@ DailyRollingFileAppender::rollover(bool alreadyLocked)
 
 
 Time
-DailyRollingFileAppender::calculateNextRolloverTime(const Time& t) const
+DailyRollingFileAppender::calculateNextRolloverTime(const Time& t_) const
 {
+    // Round down the time to whole minutes.
+    Time const t (
+        t_.getTime ()
+        - static_cast<time_t>(std::fmod (t_.getTime (), 60)));
+
     switch(schedule)
     {
-    case MONTHLY: 
+    case MONTHLY:
     {
         struct tm nextMonthTime;
         t.localtime(&nextMonthTime);
